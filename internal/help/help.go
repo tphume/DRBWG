1bd5e80b6ba6f61aa33236873341fe97075ef82d
Traphume <traphumedev@gmail.com>
2020-07-03 18:35:45 +0700

    Updated text format

--- internal/help/help.go
+++ internal/help/help.go
@@ -27,29 +27,29 @@ const (
 
 // List of brief description for sub-commands
 const (
-	atBrief     = "at - set a reminder at specified time"
-	inBrief     = "in - set a reminder for the current time + some given duration"
-	lsgBrief    = "lsg - list all the pending reminders for the current guild"
-	lscBrief    = "lsc - list all the pending reminders for the current channel"
-	updateBrief = "updated - update a pending reminder with a new specified time"
-	delBrief    = "del - delete a pending reminder"
+	atBrief     = "**at** - Set a reminder at specified time"
+	inBrief     = "**in** - Set a reminder for the current time + some given duration"
+	lsgBrief    = "**lsg** - List all the pending reminders for the current guild"
+	lscBrief    = "**lsc** - List all the pending reminders for the current channel"
+	updateBrief = "**updated** - Update a pending reminder with a new specified time"
+	delBrief    = "**del** - Delete a pending reminder"
 )
 
 // List of description for specific sub-commands
 const (
 	atDesc = "at sub-command requires 2 argument.\nthe first is a date and time in the following format - Jan-2-15:04-MST-2006.\n" +
-		"the second is a name to identify the reminder in the following format - reminderName.\nFull example - drbwg at " +
-		"Jan-2-15:04-MST-2006 reminderName"
+		"the second is a name to identify the reminder in the following format - reminderName.\nFull example - **drbwg at " +
+		"Jan-2-15:04-MST-2006 reminderName**"
 	inDesc = "in sub-command requires 2 argument.\nthe first is a duration in the following format (where the first number " +
 		"denotes the number of hours and the second denotes the minutes) - 1-20.\n" +
-		"the second is a name to identify the reminder in the following format - reminderName.\nFull example - drbwg at " +
-		"1-20 reminderName"
+		"the second is a name to identify the reminder in the following format - reminderName.\nFull example - **drbwg at " +
+		"1-20 reminderName**"
 	lsgDesc    = "lsg sub-command requires no argument"
 	lscDesc    = "lsc sub-command requires no argument"
 	updateDesc = "update sub-command requires 2 argument.\nthe first is the id of the reminder.\nthe second is either the " +
-		"duration or date and time.\nsee the at and in sub-command for the format. Full example - drbwg 432904329430 1h4m "
-	delDesc = "del sub-command requires one argument.\nthe id of the reminder to be deleted.\nFull example - drbwg delete " +
-		"32432432"
+		"duration or date and time.\nsee the at and in sub-command for the format. Full example - **drbwg 432904329430 1-4**"
+	delDesc = "del sub-command requires one argument.\nthe id of the reminder to be deleted.\nFull example - **drbwg delete " +
+		"32432432**"
 )
 
 // List of sub-commands and its description
