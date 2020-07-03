04f25430bbf215d1c21b8f6f6d1d64056119a099
Traphume <traphumedev@gmail.com>
2020-07-03 19:43:41 +0700

    Implemented Handle function

--- internal/in/in.go
+++ internal/in/in.go
@@ -2,6 +2,8 @@ package in
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 	"time"
 )
 
@@ -10,12 +12,30 @@ type Handler struct {
 }
 
 func (h *Handler) Handle(cmd []string) ([]string, error) {
-	panic("implement me")
+	if len(cmd) < 2 {
+		return nil, INVALID_INPUT
+	}
+
+	dur, name := cmd[0], strings.TrimSpace(cmd[1])
+	t, err := parse(dur, name)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := h.insert.AddReminder(t, name); err != nil {
+		return nil, err
+	}
+
+	return []string{
+		"---- **Reminder Added** ----",
+		fmt.Sprintf("**Name**: %s", name),
+		fmt.Sprintf("**Time**: %s", t),
+	}, nil
 }
 
 // Represent connection to data source
 type insertRepo interface {
-	AddRecord()
+	AddReminder(t time.Time, name string) error
 }
 
 // Helper function to validate input and return timestamp
@@ -36,6 +56,7 @@ func parse(dur string, name string) (time.Time, error) {
 
 // List of errors
 var (
+	INVALID_INPUT    = errors.New("invalid input. not enough arguments")
 	INVALID_DURATION = errors.New("invalid duration format")
 	INVALID_NAME     = errors.New("invalid reminder name")
 )
