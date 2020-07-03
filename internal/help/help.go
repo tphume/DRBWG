68c1475468ad3134b5b8523f1ee97c1660a2b368
Traphume <traphumedev@gmail.com>
2020-07-03 18:28:41 +0700

    Fixed description

--- internal/help/help.go
+++ internal/help/help.go
@@ -37,12 +37,13 @@ const (
 
 // List of description for specific sub-commands
 const (
-	atDesc = "at sub-command requires 2 argument.\nthe first is a date and time in the following format - Jan,2,15:04,MST,2006.\n" +
+	atDesc = "at sub-command requires 2 argument.\nthe first is a date and time in the following format - Jan-2-15:04-MST-2006.\n" +
 		"the second is a name to identify the reminder in the following format - reminderName.\nFull example - drbwg at " +
-		"Jan,2,15:04,MST,2006 reminderName"
-	inDesc = "in sub-command requires 2 argument.\nthe first is a duration in the following format - 1h20m.\n" +
+		"Jan-2-15:04-MST-2006 reminderName"
+	inDesc = "in sub-command requires 2 argument.\nthe first is a duration in the following format (where the first number " +
+		"denotes the number of hours and the second denotes the minutes) - 1-20.\n" +
 		"the second is a name to identify the reminder in the following format - reminderName.\nFull example - drbwg at " +
-		"1h20m reminderName"
+		"1-20 reminderName"
 	lsgDesc    = "lsg sub-command requires no argument"
 	lscDesc    = "lsc sub-command requires no argument"
 	updateDesc = "update sub-command requires 2 argument.\nthe first is the id of the reminder.\nthe second is either the " +
