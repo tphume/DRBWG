84216d866f4cd93e361513c075db3529e5a36dcc
Traphume <traphumedev@gmail.com>
2020-07-10 00:51:55 +0700

    Implemented Handle function

--- internal/lsg/lsg.go
+++ internal/lsg/lsg.go
@@ -1,6 +1,7 @@
 package lsg
 
 import (
+	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"github.com/tphume/DRBWG/internal/reminder"
 )
@@ -10,5 +11,22 @@ type Handler struct {
 }
 
 func (h *Handler) Handle(_ []string, m *discordgo.MessageCreate) ([]string, error) {
-	panic("implement me")
+	res, err := h.GuildList.ListFromGuild(reminder.GuildListArgs{GuildId: m.GuildID})
+	if err != nil {
+		return nil, err
+	}
+
+	return format(res.Data), nil
+}
+
+func format(data []reminder.Reminder) []string {
+	res := make([]string, len(data)+1)
+	
+	res[0] = fmt.Sprintf("**Retrieved a total of %d reminders for this guild**\n", len(data))
+	for i := 1; i < len(data)+1; i++ {
+		res[i] = fmt.Sprintf("**Reminder Added** :white_check_mark:\n**ID**: %s\n**Name**: %s\n**Time**: %s\n",
+			data[i].Id, data[i].Name, data[i].T)
+	}
+
+	return res
 }
