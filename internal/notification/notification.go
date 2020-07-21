2703c9fc4aef08ad64cfa8ef32ad0ffcd5a60853
Traphume <traphumedev@gmail.com>
2020-07-21 19:12:35 +0700

    Implemented remind funcion cronjob

--- internal/notification/notification.go
+++ internal/notification/notification.go
@@ -4,6 +4,8 @@ import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/robfig/cron/v3"
 	"github.com/tphume/DRBWG/internal/reminder"
+	"log"
+	"time"
 )
 
 type Process struct {
@@ -17,10 +19,30 @@ func (p *Process) AddToCron(c *cron.Cron) (cron.EntryID, error) {
 
 // Job to be run every minute
 func (p *Process) remind() {
-	panic("implement me")
+	now := time.Now()
+	start := now.Truncate(time.Minute)
+	end := now.Round(time.Minute)
+
+	// Get list of reminders
+	gpArgs := reminder.GetPendingArgs{Start: start, End: end}
+	gpRes, err := p.R.GetPending(gpArgs)
+	if err != nil {
+		panic(err)
+	}
+
+	// Send reminders to discord channels
+	if err := p.send(gpRes.Data); err != nil {
+		log.Panic(err)
+	}
+
+	// Update state of reminder
+	usArgs := reminder.StateArgs{Start: start, End: end}
+	if err := p.R.UpdateState(usArgs); err != nil {
+		log.Panic(err)
+	}
 }
 
 // Send notification
-func (p *Process) send() error {
+func (p *Process) send(r []reminder.Reminder) error {
 	panic("implement me")
 }
